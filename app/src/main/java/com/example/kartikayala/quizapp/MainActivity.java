953fc79dfd92f0a82293ea44f69559ed8dec4d19
karayala <kartik2012@gmail.com>
2016-02-22 23:05:56 -0600

    Commented

--- app/src/main/java/com/example/kartikayala/quizapp/MainActivity.java
+++ app/src/main/java/com/example/kartikayala/quizapp/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+/**
+ * Created by kartikayala
+ * Class: MainActivity (Activity)
+ * Goal : Provides the logic for the Main activity. This is the front page of the entire application.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //(Method)**************startQuiz**************//
+    //Goal: Provides functionality for when the user starts the quiz.
+    //      Moves the user to activity 2 if the user presses the "Start Quiz" button.
     public void startQuiz(View v){
         Intent transaction = new Intent(this, SecondActivity.class);
         startActivity(transaction);
